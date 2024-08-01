79d46b35cef49a5325e979de9a3d7c3d5d78dd60
vishalp2341 <vishalp2341@gmail.com>
2024-08-01 22:41:50 +0530

    /

---
+++ Programming Practice/src/Pg1.java
@@ -0,0 +1,23 @@
+class A {
+	static int i = 100;
+	
+}
+
+class B extends A {
+	static {
+		i = 200;
+	}
+}
+
+class C extends B {
+	static {
+		i = 300;
+		System.out.println(i);
+	}
+}
+
+public class Pg1 {
+	public static void main(String[] args) {
+		System.out.println(C.i);
+	}
+}
